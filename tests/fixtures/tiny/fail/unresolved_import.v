46a4ca8cdac8926080c3133294c60de410f3a594
ruwanego <ruwanego@gmail.com>
2026-06-18 09:39:32 +0530

    test(tiny): cover unresolved imports

---
+++ tests/fixtures/tiny/fail/unresolved_import.v
@@ -0,0 +1,7 @@
+// V docs: https://docs.vlang.io/module-imports.html#module-imports
+// Imported modules must be resolvable before their public definitions can be used.
+import missing_module
+
+fn main() {
+    println('unreachable')
+}
